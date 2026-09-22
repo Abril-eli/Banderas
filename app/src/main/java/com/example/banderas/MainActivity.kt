00800e9d4b0a6bae33a7f9d03c24661597eb7542
elizabeth <guillenabril02@gmail.com>
2026-09-21 20:59:09 -0600

    Primer commit

---
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.banderas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.banderas.ui.theme.BanderasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BanderasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding).fillMaxSize()
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth().weight(1f).background(Color.Green)
+                        ) { }
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth().weight(1f).background(Color.White)
+                        ) { }
+
+                        Row(modifier = Modifier.fillMaxWidth().weight(1f).background(Color.Red)) { }
+                    }
+                }
+            }
+        }
+    }
+}
