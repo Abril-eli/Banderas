4cb5806535a6215de63277fe68ad7fbe569d4540
elizabeth <guillenabril02@gmail.com>
2026-09-22 17:58:54 -0600

    package Screens

---
+++ app/src/main/java/screens/Bandera.kt
@@ -0,0 +1,36 @@
+package screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+class Bandera {
+    @Composable
+    fun BanderaScreen(modifier: Modifier= Modifier){
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth().weight(1f).background(Color.Black)
+            ) { }
+
+            Row(
+                modifier = Modifier.fillMaxWidth().weight(1f).background(Color(0xFFDD0000))
+            ) { }
+
+            Row(modifier = Modifier.fillMaxWidth().weight(1f).background(Color(0xFFFFCE00)) ){ }
+        }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun BanderaPreview(){
+        BanderaScreen()
+    }
+}
