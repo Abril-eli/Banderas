7343a896da4ba55951590e67d092ab19f2370c30
elizabeth <guillenabril02@gmail.com>
2026-09-22 17:46:56 -0600

    revision

--- app/src/main/java/com/example/banderas/MainActivity.kt
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -4,21 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.banderas.ui.theme.BanderasTheme
 
@@ -29,27 +27,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             BanderasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    Row(
+                    Column(
                         modifier = Modifier.padding(innerPadding).fillMaxSize()
                     ) {
-                        Box(
-                            modifier = Modifier.fillMaxHeight().weight(1f).background(Color(0xFF006341)),
-                                    contentAlignment = Alignment.Center
-                        ){
-                                    Image(
-                                                painter = painterResource(id = R.drawable.mexico),
-                                                contentDescription = "Escudo nacional",
-                                                modifier = Modifier.size(100.dp)
-
-                                    )
-                    }
-
-                        Box(
-                            modifier = Modifier.fillMaxHeight().weight(1f).background(Color.White)
+                        Row(
+                            modifier = Modifier.fillMaxWidth().weight(1f).background(Color.Black)
                         ) { }
 
-                        Box(modifier = Modifier.fillMaxHeight().weight(1f).background(Color(0xFFCE1126))) { }
+                        Row(
+                            modifier = Modifier.fillMaxWidth().weight(1f).background(Color(0xFFDD0000))
+                        ) { }
+
+                        Row(modifier = Modifier.fillMaxWidth().weight(1f).background(Color(0xFFFFCE00)) ){ }
                     }
                 }
             }
