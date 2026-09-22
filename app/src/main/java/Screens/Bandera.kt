74f176487106b393f09ad987212c92f89d47584d
elizabeth <guillenabril02@gmail.com>
2026-09-22 17:54:40 -0600

    package Screens

--- app/src/main/java/Screens/Bandera.kt
+++ app/src/main/java/Screens/Bandera.kt
@@ -1,4 +1,59 @@
 package Screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.banderas.R
+
 class Bandera {
+    @Composable
+    fun BanderaScreen(modifier: Modifier = Modifier) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFF006341))
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.White),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.mexico),
+                    contentDescription = "Escudo nacional",
+                    modifier = Modifier.size(100.dp)
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFCE1126))
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun BanderaPreview() {
+        BanderaScreen()
+    }
 }
