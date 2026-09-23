c7b8baf9cfb518c4300a4a37ba3c2aecbbca4739
elizabeth <guillenabril02@gmail.com>
2026-09-23 12:47:26 -0600

    package Screens

--- app/src/main/java/screens/bandera.kt
+++ app/src/main/java/screens/bandera.kt
@@ -1,13 +1,31 @@
 package screens
-
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun BanderaScreen(modifier: Modifier= Modifier){
-
-}
+fun BanderaScreen(modifier: Modifier= Modifier) {
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(Modifier.size(225.dp).clip(CircleShape).background(Color.Red)
+            )
+        }
+    }
 @Preview(showBackground = true)
 @Composable
 fun BanderaPreview(){
