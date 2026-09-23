009b59ed5b5739213fe5febf64de23df71390587
elizabeth <guillenabril02@gmail.com>
2026-09-23 12:35:55 -0600

    package Screens

--- app/src/main/java/Screens/Bamdera.kt
+++ app/src/main/java/Screens/Bamdera.kt
@@ -1,5 +1,6 @@
 
 package Screens
+import android.system.Os.close
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,19 +17,31 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
+val RombosShape = GenericShape { size, _ ->
+    moveTo(size.width / 2f, 0f)
+    lineTo(size.width, size.height / 2f)
+    lineTo(size.width / 2f, size.height)
+    lineTo(0f, size.height / 2f)
+    close()
+}
+fun lineTo(f: Float, value: Any) {}
 @Composable
 fun BanderaScreen(modifier: Modifier = Modifier) {
-    Column(Modifier.fillMaxSize()) {
-        Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFF74ACDF)))
-        Box(Modifier.weight(1f).fillMaxWidth().background(Color.White), contentAlignment = Alignment.Center
-        ) {
-            Box(Modifier.size(50.dp).clip(CircleShape).background(Color(0xFFF6B40E))
-            )
-        }
-        Box(Modifier.weight(1f).fillMaxWidth().background(Color(0xFF74ACDF)))
+    Box(modifier = modifier.fillMaxSize().background(Color(0xFF009B3A)),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(modifier = Modifier.fillMaxSize(0.75f).clip(RombosShape)
+                .background(Color(0xFFFEDF00))
+        )
+        Box(
+            modifier = Modifier
+                .size(90.dp)
+                .clip(CircleShape)
+                .background(Color(0xFF002776))
+        )
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun BanderaPreview(){
