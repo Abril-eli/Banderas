07d467537e545b1b48092297f870bc6940af2e28
elizabeth <guillenabril02@gmail.com>
2026-09-24 13:53:15 -0600

    constraint

--- app/src/main/java/Screens/Bandera.kt
+++ app/src/main/java/Screens/Bandera.kt
@@ -13,46 +13,35 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.example.banderas.R
 
-class Bandera {
     @Composable
     fun BanderaScreen(modifier: Modifier = Modifier) {
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .background(Color(0xFF006341))
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .background(Color.White),
-                contentAlignment = Alignment.Center
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.mexico),
-                    contentDescription = "Escudo nacional",
-                    modifier = Modifier.size(100.dp)
-                )
-            }
-            Box(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .background(Color(0xFFCE1126))
-            )
+        ConstraintLayout(modifier) {
+            val (caja,caja1) = createRefs()
+            Box(modifier = Modifier.size(80.dp).background(Color.Red).constrainAs(caja) {
+           start.linkTo(parent.start)
+                end.linkTo(parent.end)
+           top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+//
+            })
+            Box(modifier = Modifier.size(80.dp).background(Color.Green).constrainAs(caja1) {
+                start.linkTo(caja.start)
+                end.linkTo(caja.end)
+                top.linkTo(parent.top)
+                bottom.linkTo(caja.top)
+                verticalBias=0.3f
+                height= Dimension.fillToConstraints
+                width= Dimension.fillToConstraints
+
+            })
         }
     }
-
     @Preview(showBackground = true)
     @Composable
     fun BanderaPreview() {
-        BanderaScreen()
+        BanderaScreen(modifier = Modifier.fillMaxSize())
     }
-}
